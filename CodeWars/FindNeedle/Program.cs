4e20ef9b586eca4f34fe2e3b3719ecf0fd7e07ec
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-15 14:14:49 +0000

    Add files via upload

---
+++ CodeWars/FindNeedle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindNeedle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             static string FindNeedle(object[] haystack)
+            {
+                int index = 0;
+
+                foreach (var place in haystack)
+                {
+
+                    index++;
+                    if (place.ToString() == "needle")
+                    {
+                        break;
+                    }
+                }
+                return "needle found at position " + index.ToString();
+            }
+        }
+    }
+}
---
+++ CodeWars/RemoveLowestValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Given an array of integers, remove the smallest value. Do not mutate the original array/list.
+///Don't change the order of the elements that are left.
+/// </summary>
+namespace RemoveLowestValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveSmallest(new List<int>() { 1, 2, 3, 4, 5 });
+
+            static List<int> RemoveSmallest(List<int> numbers)
+            {
+                numbers.Remove(numbers.Min());
+                return numbers;
+
+            }
+        }
+    }
+}
