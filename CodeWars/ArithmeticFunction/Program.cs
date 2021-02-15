3136fb91f15f93667e234c7fda12be544d72ca04
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-15 16:40:35 +0000

    Add files via upload

---
+++ CodeWars/ArithmeticFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/// <summary>
+/// Given two numbers and an arithmetic operator (the name of it, as a string),
+/// return the result of the two numbers having that operator used on them.
+//a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+///The four operators are "add", "subtract", "divide", "multiply". 
+/// </summary>
+namespace ArithmeticFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static double Arithmetic(double a, double b, string op)
+            {
+                double x = 0;
+                switch (op)
+                {
+                    case "add":
+                        x = a + b;
+                        break;
+                    case "subtract":
+                        x = a - b;
+                        break;
+                    case "multiply":
+                        x = a * b;
+                        break;
+                    case "divide":
+                        x = a / b;
+                        break;
+                }
+                return x;
+            }
+        }
+    }
+}
---
+++ CodeWars/FriendOrFoe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// Make a program that filters a list of strings and returns a list with only your friends name in it.
+///If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+///Otherwise, you can be sure he's not...
+/// </summary>
+
+namespace FriendOrFoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            FriendOrFoe(names);
+            static IEnumerable<string> FriendOrFoe(string[] names)
+            {
+                List<string> friends = new List<string>();
+                foreach (string item in names)
+                {
+                    if (item.Length == 4)
+                    {
+                        friends.Add(item);
+                    }
+                }
+                return friends;
+            }
+        }
+    }
+}
