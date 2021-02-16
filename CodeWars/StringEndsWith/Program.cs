b83907ef2da7724faa7ca07409d4f32cb5c63159
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-16 12:27:16 +0000

    Add files via upload

---
+++ CodeWars/StringEndsWith/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/// <summary>
+/// Complete the solution so that it returns true
+/// if the first argument(string) passed in ends 
+/// with the 2nd argument (also a string). 
+/// </summary>
+
+namespace StringEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            static bool Solution(string str, string ending)
+            {
+              return str.EndsWith(ending);
+            }
+        }
+    }
+}
