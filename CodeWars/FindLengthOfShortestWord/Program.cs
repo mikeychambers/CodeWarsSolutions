8d1a7a98051457042af3cc0674e6861fce92e9de
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-16 11:59:57 +0000

    Add files via upload

---
+++ CodeWars/FindLengthOfShortestWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/// <summary>
+/// Given a string of words, return the length of the shortest word(s).
+/// </summary>
+namespace FindLengthOfShortestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindShort("Hello I am the lazer viking");
+            static int FindShort(string s)
+            {
+                string[] array = s.Split();
+                List<int> x = new List<int>();
+                foreach (string item in array)
+                {
+                    x.Add(item.Length);
+                }
+                return x.Min(); 
+            }
+        }
+    }
+}
