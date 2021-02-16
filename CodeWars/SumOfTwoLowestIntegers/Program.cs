8f522826672631e4f3ad48c691f8023e093d96e2
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-16 11:24:35 +0000

    Add files via upload

---
+++ CodeWars/SumOfTwoLowestIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+/// <summary>
+/// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+/// No floats or non-positive integers will be passed.
+/// </summary>
+namespace SumOfTwoLowestIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 22, 19, 12, 8 };
+
+            sumTwoSmallestNumbers(numbers);
+
+            static int sumTwoSmallestNumbers(int[] numbers)
+            {
+                int[] orderedArray = numbers.OrderBy(i => i).ToArray();
+                return orderedArray[0] + orderedArray[1];
+            }
+        }
+    }
+}
