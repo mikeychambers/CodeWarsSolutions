cdf1154046e5db0fc85726aaadaaae169b4f3d00
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2021-02-19 17:09:06 +0000

    Add files via upload

---
+++ CodeWars/NumberLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// Write a function which takes a list of strings and returns each line prepended by the correct number.
+/// </summary>
+
+namespace NumberLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string> { "a", "b", "c" };
+            Number(lines);
+            static List<string> Number(List<string> lines)
+            {
+                List<string> newLines = new List<string>();
+                int i = 0;
+                foreach (string item in lines)
+                {
+                    i++;
+                    newLines.Add(i.ToString() + ": " + item);
+                }
+
+                return newLines;
+            }
+        }
+    }
+}
